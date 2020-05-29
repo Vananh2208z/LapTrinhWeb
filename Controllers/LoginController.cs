1eaa2048afdfc4e0d4b57eeecc1f154a4ba2ad2b
nhuanh9 <chopperlouis@gmail.com>
2020-05-29 12:20:22 +0700

    add auth

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,90 @@
+﻿using BTL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL.Controllers
+{
+    public class LoginController : Controller
+    {
+        MSME2 db = new MSME2();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LoginForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    db.accounts.SingleOrDefault(x => x.username == model.username && x.password == model.password);
+                if (result != null)
+                {
+                    CustomerModel customer = new CustomerModel()
+                    {
+                        TenTK = result.username
+                    };
+                    Session["UserName"] = result.username;
+                    Session["Role"] = result.role.ToString();
+                   
+                    Session.Add(SessionContants.LOGIN, customer);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập thất bại");
+                }
+            }
+
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAcount(account model)
+        {
+            int id = db.accounts.ToList().Count;
+            if (ModelState.IsValid)
+            {
+                model.id = id;
+                model.role = null;
+                db.accounts.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult QLTK()
+        {
+            if(Session["UserName"] == null)
+            {
+                return RedirectToAction("LoginForm", "Login");
+            }
+            return View(db.accounts.ToList());
+        }
+
+    }
+}
---
+++ Controllers/TimKiemController.cs
@@ -0,0 +1,34 @@
+﻿using BTL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+namespace BTL.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        MobileStoreManagementEntities1 db = new MobileStoreManagementEntities1();
+        // GET: TimKiem
+        public ActionResult KetQuaTimKiem(FormCollection form, int? page)
+        {
+            int pageSize = 100;
+            int pageNumber = (page ?? 1);
+            string searchKey = form["txtTimKiem"].ToString();
+            ViewBag.form = form;
+            List<SanPham> lstKQTK = db.SanPhams.Where(n => n.TenSP.Contains(searchKey)).ToList();
+            if (lstKQTK.Count == 0)
+            {
+                ViewBag.ThongBao = "Không tìm thấy sản phẩm nào!";
+                return View(db.SanPhams.ToList().ToPagedList(pageNumber, pageSize));
+            }
+            else
+            {
+                ViewBag.ThongBao = "Đã tìm thấy " + lstKQTK.Count() + " sản Phẩm "+searchKey;
+                return View(lstKQTK.ToList().ToPagedList(pageNumber, pageSize));
+            }
+        }
+    }
+}
